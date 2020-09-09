d7c04fddca5469edb336cd99a8b8d9b0fc67de71
grebenuks <grebenuks2406@gmail.com>
2020-09-09 16:41:11 +0300

    fixed Spinner

--- src/components/Spinner/Spinner.js
+++ src/components/Spinner/Spinner.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Loader from 'react-loader-spinner';
+import styles from '../../styles.module.css';
 
 export function Spinner() {
   return (
-    <>
+    <div className={styles.BtnWrap}>
       <Loader type="ThreeDots" color="#303f9f" height={50} width={50} />
-    </>
+    </div>
   );
 }
